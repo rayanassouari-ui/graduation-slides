acd48ea7b74c0b30c44ac3cfbbe55a34fa2fb819
rayanassouari-ui <rayanassouari@gmail.com>
2026-06-11 17:10:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,42 @@ STUDENTS = [
         "school": "High School in the Community",
         "description": "To be added..."
     },
+     {
+        "name": "Ahmad Amirzai",
+        "photo": "/static/photos/student3.jpg",
+        "school": "HillHOUSE",
+        "description": "To be added..."
+    },
+    {
+        "name": "Rayan Assouari",
+        "photo": "/static/photos/student3.jpg",
+        "school": "High School in the Community",
+        "description": "To be added..."
+    },
+      {
+        "name": "Yeimy Carisa Barreda",
+        "photo": "/static/photos/student3.jpg",
+        "school": "Wilbur Cross",
+        "description": "To be added..."
+    },
+       {
+        "name": "Aisake Levon Brandon",
+        "photo": "/static/photos/student3.jpg",
+        "school": "Career",
+        "description": "To be added..."
+    },
+     {
+        "name": "Nicholai Josiah Oponde Brown",
+        "photo": "/static/photos/student3.jpg",
+        "school": "COMMON GROUND",
+        "description": "To be added..."
+    },
+     {
+        "name": "Leeyanna Caple",
+        "photo": "/static/photos/student3.jpg",
+        "school": "Wilbur Cross",
+        "description": "To be added..."
+    },
 ]
 
 CLASS_YEAR = "Class of 2026"
