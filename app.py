1dd2b71bf4ecf5f59dfbfca3c095d491c6980c45
rayanassouari-ui <rayanassouari@gmail.com>
2026-06-22 13:45:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -221,16 +221,16 @@ STUDENTS = [
         "description": " Este título lleva tres nombres grabados en el corazón: Dios, quien me dio la vida y las fuerzas; y mis padres, quienes me dieron las alas para volar."
     },
       {
-        "name": "Jeremiah Amir Ragsdale",
-        "photo": "https://scontent-bos5-1.xx.fbcdn.net/v/t1.15752-9/721859909_2112063449356095_1724427522335985681_n.png?_nc_cat=109&ccb=1-7&_nc_sid=fc17b8&_nc_ohc=lQlKrZsd-10Q7kNvwGZX8pi&_nc_oc=AdrmBLePbaB2e6a6ddP01lSjA-suq2b2O8I9FkIT-jZAhBT-Yw1eyiw7DyKihZKo6TDFoPJAl_-8--RALnqjbYJQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-bos5-1.xx&_nc_ss=7a22e&oh=03_Q7cD5gGb6Gc6w6At-JkQUI7k9lhpuv6V3_XklBXDmyI2cfiiGQ&oe=6A53E531",
-        "school": "CAREER",
-        "description": " "
-    },
-     {
         "name": "Angie Porter",
         "photo": "https://scontent-bos5-1.xx.fbcdn.net/v/t1.15752-9/721733559_1548682056958635_5597990434896591474_n.png?_nc_cat=109&ccb=1-7&_nc_sid=fc17b8&_nc_ohc=SeqCtNPm3UUQ7kNvwGP63ND&_nc_oc=AdqMq1Jj-6xH3GqKMpS7L2I8p6-nWwGDyghg5cNA_aTIX3mEulaZbxAeFcr-YOMtP6ClYsEPwlcHCKgYlnrA1VQZ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-bos5-1.xx&_nc_ss=7a22e&oh=03_Q7cD5gEpM3E7MnsiNCLy-aDOgm2JeR84Fks1ko2kOISqFGLxwQ&oe=6A53C580",
         "school": "HILLHOUSE",
         "description": "This is just the first step"
+    },
+      {
+        "name": "Jeremiah Amir Ragsdale",
+        "photo": "https://scontent-bos5-1.xx.fbcdn.net/v/t1.15752-9/721859909_2112063449356095_1724427522335985681_n.png?_nc_cat=109&ccb=1-7&_nc_sid=fc17b8&_nc_ohc=lQlKrZsd-10Q7kNvwGZX8pi&_nc_oc=AdrmBLePbaB2e6a6ddP01lSjA-suq2b2O8I9FkIT-jZAhBT-Yw1eyiw7DyKihZKo6TDFoPJAl_-8--RALnqjbYJQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-bos5-1.xx&_nc_ss=7a22e&oh=03_Q7cD5gGb6Gc6w6At-JkQUI7k9lhpuv6V3_XklBXDmyI2cfiiGQ&oe=6A53E531",
+        "school": "CAREER",
+        "description": " "
     },
        {
         "name": "Mirwais Rahmani",
